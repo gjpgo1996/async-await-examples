3c5d0e1a979192cf068f192df1fc2f12912ed86c
gjpgo1996 <gjpgot@gmail.com>
2020-03-30 17:47:44 +0000

    aprendiendo js con nodejs async away

--- explanations/promises.js
+++ explanations/promises.js
@@ -22,3 +22,21 @@ function requestHandler(req, res) {
     // whatever code
 
 }
+
+function requestHandler(req, res) {
+    User.findById(req.userId)
+        .then(function(user) {
+            return Tasks.findById(user.tasksId)
+        })
+        .then(function(tasks) {
+            tasks.completed = true;
+            return tasks.save();
+        })
+        .then(function() {
+            res.send('Tasks completed');
+        })
+        .catch(function(errors) {
+            res.send(errors);
+        })
+
+}
