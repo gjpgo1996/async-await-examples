aa17e1632c96359c62b63a92560a6dd5891c3bde
gjpgo1996 <gjpgot@gmail.com>
2020-03-30 19:30:23 +0000

    ejecutar de manera paralela y secuencial dos métodos

---
+++ sequential/index.js
@@ -0,0 +1,17 @@
+const { taskOne, taskTwo } = require('./tasks');
+
+async function main() {
+    try {
+        console.time('Measuring time');
+        const results = await Promise.all([taskOne(), taskTwo()])
+        
+        console.timeEnd('Measuring time');
+
+        console.log('Task One returned', results[0]);
+        console.log('Task Two returned', results[1]);
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+main();
